d39969b9f0573d23902e2fafe71532440bfb1b21
chun2879 <chun2879@gmail.com>
2025-11-17 14:35:51 +0900

    Create StateRecorder.cs

---
+++ 12주차/StateRecorder.cs
@@ -0,0 +1,91 @@
+using UnityEngine;
+using System.Collections.Generic;
+
+[System.Serializable]
+public class RecordedFrame
+{
+    public float time;
+
+    public Vector3 ikPosition;
+    public List<Quaternion> ghostRotations = new List<Quaternion>();
+
+    public Vector3 cubePosition;
+    public Quaternion cubeRotation;
+
+    public bool cubeHeld;
+
+    // 의사 토크 스냅샷
+    public List<float> pseudoTorques = new List<float>();
+}
+
+public class StateRecorder : MonoBehaviour
+{
+    private List<RecordedFrame> frames = new List<RecordedFrame>();
+
+    public bool HasData => frames.Count > 0;
+    public float MaxTime => frames.Count > 0 ? frames[frames.Count - 1].time : 0f;
+
+    public void Clear()
+    {
+        frames.Clear();
+    }
+
+    public void RecordFrame(
+        float time,
+        Transform ikTarget,
+        List<Transform> ghostJoints,
+        Transform cube,
+        bool cubeHeld,
+        PseudoTorqueHeatmap heatmap)
+    {
+        RecordedFrame f = new RecordedFrame();
+        f.time = time;
+
+        // IK
+        f.ikPosition = ikTarget.position;
+
+        // 관절 회전
+        foreach (var j in ghostJoints)
+            f.ghostRotations.Add(j.localRotation);
+
+        // 큐브
+        if (cube != null)
+        {
+            f.cubePosition = cube.position;
+            f.cubeRotation = cube.rotation;
+        }
+
+        f.cubeHeld = cubeHeld;
+
+        // 히트맵 토크 기록
+        if (heatmap != null && heatmap.joints != null)
+        {
+            for (int i = 0; i < heatmap.joints.Count; i++)
+            {
+                f.pseudoTorques.Add(heatmap.GetSmoothedTorque(i));
+            }
+        }
+
+        frames.Add(f);
+    }
+
+    /// t보다 같거나 작은 프레임 중 가장 마지막 것 (이전 프레임) 반환
+    public RecordedFrame GetFrameAtTime(float t)
+    {
+        if (frames.Count == 0) return null;
+
+        int left = 0;
+        int right = frames.Count - 1;
+
+        while (right - left > 1)
+        {
+            int mid = (left + right) / 2;
+            if (frames[mid].time < t)
+                left = mid;
+            else
+                right = mid;
+        }
+
+        return frames[left];
+    }
+}
